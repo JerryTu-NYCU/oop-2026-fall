57468e251e9e21ce8cd5ff5aa14d8c9dc482f31d
JerryTu <jerrytu0916@gmail.com>
2026-09-21 19:05:30 +0800

    finishing test cases and removing bounds check on avg score

--- 112613017/lab03/test_grade.py
+++ 112613017/lab03/test_grade.py
@@ -15,19 +15,28 @@ def test_letter_grade_valid_scores(score, expected):
     assert letter_grade(score) == expected
 
 
+def test_letter_grade_score_bounds_l():
+    with pytest.raises(ValueError):
+        letter_grade(-1)
+
+
+def test_letter_grade_score_bounds_h():
+    with pytest.raises(ValueError):
+        letter_grade(101)
+
+
 def test_average():
-    l = [12, 35, 77, 95]
+    l = [80, 90, 100]
     # I use the average to test the average
     # Could actually blow up in my face due to floating point error
     assert average_scores(l) == mean(l)
 
 
-def test_average_e():
+def test_average_error():
     with pytest.raises(ValueError):
         average_scores([])
 
 
-def test_average_e2():
-    with pytest.raises(ValueError):
-        average_scores([114, 51, 4])
-
+# def test_average_e2():
+#     with pytest.raises(ValueError):
+#         average_scores([114, 51, 4])
